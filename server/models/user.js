e7433cbcb77c92239ebebd8c1074d21ac379038f
Eslam Ahmed <eabusleem@2pegypt.local>
2023-01-15 14:33:29 +0200

    added user model

---
+++ server/models/user.js
@@ -0,0 +1,44 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../DB/dbConfig')
+
+const User = sequelize.define('User',{
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+        unique: true
+    },
+    username:{
+        type: DataTypes.STRING(45),
+        allowNull: false,
+    },
+    email:{
+        type: DataTypes.STRING(45),
+        allowNull: false,
+    },
+    password:{
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    name:{
+        type: DataTypes.STRING(45),
+        allowNull: false,
+    },
+    coverPic:{
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    profilePic:{
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    city:{
+        type: DataTypes.STRING(45),
+        allowNull: true,
+    },
+    website:{
+        type: DataTypes.STRING(45),
+        allowNull: true,
+    },
+})
