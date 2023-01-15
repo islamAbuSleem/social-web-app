4e3716a2f815ba5a4c6215dadace453bd0e27fa4
Eslam Ahmed <eabusleem@2pegypt.local>
2023-01-15 14:03:53 +0200

    added mysql connection

--- server/index.js
+++ server/index.js
@@ -2,9 +2,20 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const morgan = require('morgan');
+const sequelize = require('./DB/dbConfig')
+
 
 app.use(cors);
 app.use(morgan('tiny'));
+
+// testing db connection
+    sequelize.authenticate().then(()=>{
+        console.log('Connection has been established successfully.');
+    }).catch((e)=>{
+        console.error('Unable to connect to the database:', e);
+    })
+    
+
 
 
 
