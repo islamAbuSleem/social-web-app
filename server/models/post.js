49f7d3997e76f55e2d6a2030a8bf6ca1658cc863
Eslam Ahmed <eabusleem@2pegypt.local>
2023-01-15 14:52:14 +0200

    Added post model

---
+++ server/models/post.js
@@ -0,0 +1,30 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../DB/dbConfig')
+
+const User = require('./user');
+
+const Post = sequelize.define('Post',{
+    id:{
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        unique: true,
+        primaryKey: true
+    },
+    desc:{
+        type: DataTypes.STRING,
+    },
+    image:{
+        type: DataTypes.STRING,
+    },
+    userId:{
+        type: DataTypes.INTEGER,
+        references:{
+            model: User,
+            key: 'id'
+        }
+    }
+})
+
+Post.hasOne(User,{onDelete: 'cascade', hooks: true})
+
+module.exports = Post
