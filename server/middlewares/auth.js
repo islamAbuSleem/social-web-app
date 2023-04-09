6a992958e497c2e6cda47c8a5def5bb55641bc02
islam <aaasalim@dttmoj.onmicrosoft.com>
2023-04-09 11:43:07 +0200

    added multer tto upload images and auth middleware

---
+++ server/middlewares/auth.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken');
+
+module.exports.verifyUser = (req, res, next) => {
+    const token = req.cookies.accessToken;
+    if (!token) return res.status(401).send('please login to add post');
+
+    try {
+        var decoded = jwt.verify(token, process.env.jwtPrivteKey);
+        if (decoded) {
+            return next()
+        }
+    } catch (err) {
+        return res.status(401).send('invalid token')
+    }
+}
